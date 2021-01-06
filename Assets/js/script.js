d031584ca554c0473ff558e031b263b4806d0155
Seth Zyg <zygster11@gmail.com>
2021-01-05 23:28:52 -0600

    Penultimate commit. Worked out bugs, annotated files, and restructured files as per best practice.

---
+++ Assets/js/script.js
@@ -0,0 +1,85 @@
+
+//This variable is set to the button labeled "Generate Password" in the HTML. It will be 
+var generateButton = document.querySelector("#passwordButton");
+
+//This the main function of the button, which will be referenced in the generateButton.addEventListener() at the end of the script.
+var passwordGenerator = function() { 
+
+/*Basically, this while loop will continue cycling until the user inputs a valid integer between 8 and 128, inclusive.
+i.e., if the user chooses 5, it will alert the user that their choice was invaled and prompt the user again for another choice. 
+Likewise, if the user enters any string, it will once again show the alert and prompt the user for a valid number. */
+    while (true) {
+
+        //passwordLength will equal the total length of the user's generated password.
+        var passwordLength = prompt("How many characters will your password be? Choose a number from 8 to 128 inclusive.")
+
+        if (passwordLength <= 128 && passwordLength >= 8) {
+           alert("Sweet! Your password will be "+passwordLength+" characters long!");
+           parseInt(passwordLength); //Not sure that I need this but it's here just in case.
+            break;
+                
+        } else {
+           alert("Please enter a valid integer between 8 and 128 inclusive!");
+        }
+        }
+    
+    /*These variables represent potential characters the user will choose from. The empty string passwordCharacters will eventually
+    become the pool of characters from which the password will be generated.
+    The potentialCharacters will be used in a for loop to ask the user, in turn, if they wish for those characters to be included in the pool.*/
+    var passwordCharacters = "";
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numberSet = "0123456789";
+    var specialSet = "~!@#$%^&*?";
+    var potentialCharacters = [lowercase, uppercase, numberSet, specialSet];
+   
+    
+    while (passwordCharacters === "") { 
+        /*so while passwordCharacters is still empty, stay in the loop! This ensures the user chooses AT LEAST ONE
+        of the set of potential characters in the array. Otherwise, the computer will keep asking for them. */
+     for (var i = 0; i < potentialCharacters.length; i++) {
+
+         /*this for loop and confirm statement combo will cycle through the potentialCharacters array, asking the user if they want that type of 
+         character included in their password. */
+        var userChoice = confirm("Do you want the following as potential characters in your password?: "+potentialCharacters[i]);
+        
+        if (userChoice) {
+            /*if the user DOES want that type of character in the password, then passwordCharacters will add that string of characters into its string*/
+            passwordCharacters += potentialCharacters[i];
+             //just confirming to the user that we will include those characters in the password.
+            alert("Radical! We'll include "+potentialCharacters[i]+" in your password!");
+        }   
+        else { //This alert happens if they decline to include the specified character set in their password.
+            //This alert and loop will keep happening until the user selects at least one of the options.
+            alert("No worries, mate. Just please select at least one of these options!");
+        }
+    }
+}
+           
+    //declaring a variable equal to the length of the string of potential password characters as chosen by the user in above while loop
+    var n = passwordCharacters.length; 
+
+    //this empty string will eventually become the user's password
+    var actualPassword = ""; 
+
+    /*So basically, below code says that for i starting at zero and running until passwordLength - 1 (i.e. it will iterate 'passwordLength' times, as desired by user), 
+    it will add a random character from the list of passwordCharacters, created just above by the user, to the empty string 'actualPassword'. */
+    for (var i = 0; i < passwordLength; i++) { 
+        actualPassword += passwordCharacters.charAt(Math.floor(Math.random()* n));
+    }
+        
+    
+    
+//Just some cute lil alerts.
+alert("Drumroll, please, while Password Generator: 5000 works its magic! bdrbdrbdrbdrbdrbdrbdr....")
+alert("Awwww SNAP! Your new password is ready! Write it down so you don't forget it like I always do!");
+
+//this prints the password into the container labeled "Your New Password"
+document.querySelector(".passwordContainer").innerHTML = actualPassword; 
+
+return actualPassword;  //not even sure I need this lolol
+
+}
+
+//This is the main event! Literally! When the Generate Password button is clicked in HTML, that big ol' function above does its thang!
+generateButton.addEventListener("click", passwordGenerator);
---
+++ MyREADME.md
@@ -0,0 +1,40 @@
+# Password Generator - Homework 3
+
+An application that generates a random password for the user, built by Seth Zygarlicke.
+
+# Purpose of Repository
+
+This is the third required homework assignment for the boot camp. The project necessitated the use of Javascript to create a random password generator using input from the user. After generating the password, the application prints it to the screen for the user to use. The application must work on all screen sizes and require accurate and sufficient input from the user.
+
+# Description
+
+The repository consists of three main components - the html, the css, and the javascript. The html is fairly simple consisting of a header that labels the page 'Password Generator: 5000', a container holding all of the relevant items for the password generation, and cool space-themed background image. 
+
+The main feature of the html page is the button labeled "Generate Password". Upon click, this button will launch the function elaborated in the javascript file. This function will prompt the user to input a number between 8 and 128 inclusive. If the user fails to do so (e.g. with a 5 or a string), the browser will continue asking until a valid integer is submitted. This will be the password length.
+
+Once a length is established, the browser then blasts the user with a series of prompts. These prompts ask the user, in turn, if they wish to include the set of lowercase letters, the set of uppercase letters, the set of numeric chracters, and/or a set of special characters. The broswer requires the user to choose at least one of these set of characters, otherwise it will continue blasting the user with prompts to choose a set.
+
+The user's choices of characters will be pooled together. The function then chooses, randomly, a set of characters from this pool equal to the length chosen by the user in the first prompt. Upon completion, the password will print into the dashed box in the middle of the main container for the user to copy and use at their leisure. 
+
+# Files in Repository
+
+The index.html file contains the base code for the webpage and stands outside any other folders. The style.css file will be found in the 'css' folder which is housed inside the 'Assets' folder. Similarly, the script.js file is found in its 'js' folder which rests inside the 'Assets' folder. 
+
+Also included in the Assets folder is the 'demo' picture for this assignment, the cool space-background image, and screenshots of the created page: one at base (Password-Generator-Base.png) and another after a random 128-character password has been generated and printed to the page. These screenshots are shown below.
+
+The Develop folder can be largely ignored. It contains the files given as examples to get the ball rolling in this assignment. They have no bearing on the created application, but were left in for the sake of completeness by Seth. 
+
+# Links and Screenshots
+
+https://elderbass.github.io/Password_Generator/
+
+https://github.com/ElderBass/Password_Generator.git
+
+
+Before password is generated:
+
+![Password Generator at base](./Assets\Password-Generator-Base.png "No Password")
+
+After a 128-character password is generated:
+
+![Password Generator with a rand 128 character password](./Assets\Generator-With-128-Password.png "With 128 Password" )
